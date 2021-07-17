a797580076e88be1b6875b3a6c18edcb486b13f8
Daniele Pensiero <daniele.pensiero@gmail.com>
2021-07-17 17:36:33 +0200

    complex vision

--- scripts/BasicAi.gd
+++ scripts/BasicAi.gd
@@ -1,21 +1,14 @@
 extends "res://scripts/AbstractController.gd"
 
-export(PackedScene) var Patrol 
 export(int) var OffsetJump = 10
 
-var patrol_follow : PathFollow2D
-func _ready():
-	var patrol_inst = Patrol.instance()
-	get_tree().root.get_child(0).call_deferred("add_child", patrol_inst)
-	patrol_follow = patrol_inst.get_child(0)
-
 func think(_delta):
-	direction = ( patrol_follow.position - character.position ).normalized()
-	points_to = patrol_follow.position
+	direction = ( character.patrol_follow.position - character.position ).normalized()
+	points_to = character.patrol_follow.position
 	
-	if character.position.distance_to(patrol_follow.position) < 50:
-		patrol_follow.offset += OffsetJump
+	if character.position.distance_to(character.patrol_follow.position) < 50:
+		character.patrol_follow.offset += OffsetJump
 
 func do():
 	character.walk()
-	character.look_at(patrol_follow.position)
+	character.look_at(character.patrol_follow.position)
