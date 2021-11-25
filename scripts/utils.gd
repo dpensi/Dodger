db08aaa204f04cf2453bc14610e8af17b52f1345
Daniele Pensiero <daniele.pensiero@gmail.com>
2021-11-25 19:02:21 +0100

    better level

--- scripts/utils.gd
+++ scripts/utils.gd
@@ -16,3 +16,12 @@ func vector2_average(vectors):
 
 func middle_point(vec1, vec2):
 	return vector2_average([vec1, vec2])
+
+func almost_eq(val1, val2, epsilon=0.1 ):
+	return abs(val1 - val2) < epsilon
+
+func almost_gt(val1, val2, epsilon=0.1 ):
+	return val1 - val2 > epsilon
+	
+func almost_lt(val1, val2, epsilon=0.1 ):
+	return not almost_gt(val1, val2, epsilon)
