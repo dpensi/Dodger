567ab275c06296a375ea07e8b154be9cf7c6d2c7
Daniele Pensiero <daniele.pensiero@gmail.com>
2021-11-26 18:37:46 +0100

    abstract weapon

--- weapons/gun/Gun.gd
+++ weapons/gun/Gun.gd
@@ -1,17 +1,21 @@
-extends RigidBody2D
+extends "res://weapons/abstract/Weapon.gd"
 
 export(float) var AllowedFiringDistance = 80
 export(PackedScene) var Bullet
  
 var item_id = "gun"
 
-func fire():
-	if get_global_mouse_position().distance_to(global_position) \
-		< AllowedFiringDistance:
-		print("aiming too close!")
-		return
-		
-	var b = Bullet.instance()
-	b.direction = (get_global_mouse_position() - global_position).normalized()
-	b.position = global_position
-	get_tree().current_scene.add_child(b)
+func attack(_body):
+	if can_attack(_body):
+		var b = Bullet.instance()
+		b.direction = (get_global_mouse_position() - global_position).normalized()
+		b.position = global_position
+		get_tree().current_scene.add_child(b)
+	
+		.attack(_body)
+
+func can_attack(_body):
+	return .can_attack(_body) and \
+		not (get_global_mouse_position().distance_to(global_position) \
+		< AllowedFiringDistance)
+	
